071465ff597604591df8cff901b216b48855d4f9
Jack Staffen <jack.staffen@gmail.com>
2019-10-02 16:33:01 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -50,5 +50,4 @@ def checkout(cart, coupons)
   applied_coupons = apply_coupons(consolidated_cart, coupons)
   applied_clearance = apply_clearance(applied_coupons)
   total = applied_clearance.reduce(0) {|sum, (key, value)| sum + value[:price]}
-  total -= (total * 0.10).round(2) if total > 100
 end

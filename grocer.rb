a1472d923c07a4d0386fb6b225cd2d9c436e613c
Jack Staffen <jack.staffen@gmail.com>
2019-10-02 16:25:58 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -50,9 +50,5 @@ def checkout(cart, coupons)
   #binding.pry
   applied_coupons = apply_coupons(consolidated_cart, coupons)
   applied_clearance = apply_clearance(applied_coupons)
-  applied_clearance.each do |key, value|
-    num = applied_clearance[key][:price]
-    applied_clearance.reduce(0) {|sum, num| sum + num}
-  end
-  sum
+  
 end

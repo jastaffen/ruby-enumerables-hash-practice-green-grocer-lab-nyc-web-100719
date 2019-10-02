7ebc3f188ed4f9087d314f48583158459811f0ba
Jack Staffen <jack.staffen@gmail.com>
2019-10-02 16:16:11 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -47,4 +47,5 @@ end
 def checkout(cart, coupons)
   consolidated_cart = consolidate_cart(cart)
   applied_coupons = apply_coupons(consolidated_cart, coupons)
+  applied_clearance = apply_clearance(applied_coupons)
 end
